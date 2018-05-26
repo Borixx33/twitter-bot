246ad4dc5d4b59c87aa4bf3ac96638f6a6afb3b1
paulhiv <paul.hivert@ynov.com>
2018-05-26 13:53:27 +0200

    Add project files.

---
+++ twitter-bot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twitter_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
